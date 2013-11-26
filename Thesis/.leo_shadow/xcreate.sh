69f2b83b6cb1ef37842a1462bdcad8cb8239d628
Paran Haslett <paranhaslett@gmail.com>
2013-11-26 14:09:02 +1300

    Cleanup, reorgainise + draft proposal

--- Thesis/.leo_shadow/xcreate.sh
+++ Thesis/.leo_shadow/xcreate.sh
@@ -3,6 +3,7 @@
 #@@language shell
 #!/bin/csh
 fmpp.sh
+texworks ./out/Tex/Proposal.tex
 cp ./out/public_html/* ~/public_html/
 chmod 604 ~/public_html/*
 
--- Thesis/create.sh
+++ Thesis/create.sh
@@ -1,5 +1,6 @@
 #!/bin/csh
 fmpp.sh
+texworks ./out/Tex/Proposal.tex
 cp ./out/public_html/* ~/public_html/
 chmod 604 ~/public_html/*
 
